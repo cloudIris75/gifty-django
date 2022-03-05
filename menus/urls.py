5edf2b63c00a2db36869a002ae691eb9481b9214
Iris <gmyeom1230@gmail.com>
2022-03-05 16:47:27 +0900

    Complete brand filtering

--- menus/urls.py
+++ menus/urls.py
@@ -4,8 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('brands/', views.BrandListView.as_view(), name='brand-list'),
-    path('menus/', views.MenuListView.as_view(), name='menu-list'),
+    path('menus/', views.menu_list, name='menu-list'),
     path('menus/<int:id>/', views.menu_brand, name='menu-brand'),
-    # path('menus/<int:id, str:category>/' views.menu_category, name='menu_category'),
     path('calculator/', views.calculator, name='calculator')
 ]
--- menus/views.py
+++ menus/views.py
@@ -10,13 +10,31 @@ class BrandListView(ListView):
     model = Brand
     ordering = ['id']
 
-class MenuListView(ListView):
-    model = Menu
-    ordering = ['id']
-    paginate_by = 6
+def menu_list(request):
+    context = dict()
+
+    brands = Brand.objects.all()
+    context['brands'] = brands
+
+    menu = Menu.objects.all()
+    context['menu_list'] = menu
+
+    paginator = Paginator(menu, 6)
+    curr_page_num = request.GET.get('page')
+    if curr_page_num is None:
+        curr_page_num = 1
+    page = paginator.page(curr_page_num)
+    context['page_obj'] = page
+
+    return render(request, 'menus/menu_list.html', context=context)    
 
 def menu_brand(request, id):
     context = dict()
+    context['id'] = id
+
+    brands = Brand.objects.all()
+    context['brands'] = brands
+
     menu_brand = Menu.objects.filter(brand_id=id)
     context['menu_list'] = menu_brand
 
@@ -28,10 +46,6 @@ def menu_brand(request, id):
     context['page_obj'] = page
 
     return render(request, 'menus/menu_list.html', context=context)
-
-# def menu_category(request, id, category):
-#     menu_category = Menu.objects.filter(brand_id=id, category=category)
-#     return render(request, 'menus/menu_list.html', {'menu_category': menu_category})
 
 def calculator(request):
     return render(request, 'menus/calculator.html')
