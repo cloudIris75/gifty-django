2ee492391c40539fbc0c88851385efc547a34179
Iris <gmyeom1230@gmail.com>
2022-03-05 14:59:39 +0900

    Add brand filter in menu

--- menus/urls.py
+++ menus/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('brands/', views.BrandListView.as_view(), name='brand-list'),
     path('menus/', views.MenuListView.as_view(), name='menu-list'),
-    # path('menus/<str:pk>/', views.menu_brand, name='menu-brand'),
+    path('menus/<int:id>/', views.menu_brand, name='menu-brand'),
+    # path('menus/<int:id, str:category>/' views.menu_category, name='menu_category'),
     path('calculator/', views.calculator, name='calculator')
 ]
--- menus/views.py
+++ menus/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.core.paginator import Paginator
 from menus.models import Brand, Gifticon, Menu
 
 def index(request):
@@ -14,9 +15,23 @@ class MenuListView(ListView):
     ordering = ['id']
     paginate_by = 6
 
-# def menu_brand(request, pk):
-#     menu = Menu.objects.get(brand=pk)
-#     return render(request, 'menus/menu_list.html', {'menu': menu})
+def menu_brand(request, id):
+    context = dict()
+    menu_brand = Menu.objects.filter(brand_id=id)
+    context['menu_list'] = menu_brand
+
+    paginator = Paginator(menu_brand, 6)
+    curr_page_num = request.GET.get('page')
+    if curr_page_num is None:
+        curr_page_num = 1
+    page = paginator.page(curr_page_num)
+    context['page_obj'] = page
+
+    return render(request, 'menus/menu_list.html', context=context)
+
+# def menu_category(request, id, category):
+#     menu_category = Menu.objects.filter(brand_id=id, category=category)
+#     return render(request, 'menus/menu_list.html', {'menu_category': menu_category})
 
 def calculator(request):
     return render(request, 'menus/calculator.html')
