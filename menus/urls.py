6bd232bb95a548eb541545c843b4ddeb76b0b58d
Iris <gmyeom1230@gmail.com>
2022-03-06 13:56:39 +0900

    Complete category filtering

--- menus/urls.py
+++ menus/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('brands/', views.BrandListView.as_view(), name='brand-list'),
     path('menus/', views.menu_list, name='menu-list'),
     path('menus/<int:id>/', views.menu_brand, name='menu-brand'),
+    path('menus/<int:id>/<str:ct>/', views.menu_category, name='menu-category'),
     path('calculator/', views.calculator, name='calculator')
 ]
--- menus/views.py
+++ menus/views.py
@@ -16,7 +16,7 @@ def menu_list(request):
     brands = Brand.objects.all()
     context['brands'] = brands
 
-    menu = Menu.objects.all()
+    menu = Menu.objects.order_by('brand_id', 'category')
     context['menu_list'] = menu
 
     paginator = Paginator(menu, 6)
@@ -39,6 +39,26 @@ def menu_brand(request, id):
     context['menu_list'] = menu_brand
 
     paginator = Paginator(menu_brand, 6)
+    curr_page_num = request.GET.get('page')
+    if curr_page_num is None:
+        curr_page_num = 1
+    page = paginator.page(curr_page_num)
+    context['page_obj'] = page
+
+    return render(request, 'menus/menu_list.html', context=context)
+
+def menu_category(request, id, ct):
+    context = dict()
+    context['id'] = id
+    context['ct'] = ct
+
+    brands = Brand.objects.all()
+    context['brands'] = brands
+
+    menu_category = Menu.objects.filter(brand_id=id, category=ct)
+    context['menu_list'] = menu_category
+
+    paginator = Paginator(menu_category, 6)
     curr_page_num = request.GET.get('page')
     if curr_page_num is None:
         curr_page_num = 1
