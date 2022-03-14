f0eb3b38f2dca45e896e0904b65b6690876d6819
Iris <gmyeom1230@gmail.com>
2022-03-14 16:28:30 +0900

    Highlight current page

--- menus/static/menus/js/sidebar.js
+++ menus/static/menus/js/sidebar.js
@@ -1,1 +1,18 @@
 const url = window.location.pathname;
+const links = document.getElementsByClassName('link');
+
+switch(url) {
+    case '/':
+        links[0].classList.toggle('active');
+        break;
+    case '/brands/':
+        links[1].classList.toggle('active');
+        break;
+    case '/menus/':
+        links[2].classList.toggle('active');
+        break;
+    case '/calculator/':
+        links[3].classList.toggle('active');
+        break;
+}
+
