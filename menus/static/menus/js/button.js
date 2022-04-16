ce3396bd2828d32add91658f6e515e2b0c13fa1d
Iris <gmyeom1230@gmail.com>
2022-04-16 18:31:03 +0900

    Modify down button pos

--- menus/static/menus/js/button.js
+++ menus/static/menus/js/button.js
@@ -1,13 +1,15 @@
-const wrap = document.getElementById('wrap');
+const html = document.getElementById('html');
 const button = document.getElementById('down');
 
-button.onclick = function() {
-    wrap.scroll({
-        top: wrap.scrollHeight,
-        left: 0,
-        behavior: 'smooth'
-    });
-};
+if (button) {
+    button.onclick = function() {
+        html.scroll({
+            top: html.scrollHeight,
+            left: 0,
+            behavior: 'smooth'
+        });
+    };
+}
 
 function view() {
     document.getElementById('message').classList.toggle('active');
